3aba51b9e35de88c35c2a4369576e52f731b2ec1
HenriMertens <149707229+HenriMertens@users.noreply.github.com>
2024-07-31 20:32:43 +0200

    cleanup

--- rev/hard/checkpass/checkpass/brute.py
+++ rev/hard/checkpass/checkpass/brute.py
@@ -1,5 +1,5 @@
 from pwn import *
 for i in range(64):
-    #Run binary with increasing length argument
+    #Run binary with increasing length argument.
     p = process(['./checkpass', i *'A'])
     print('character length {}'.format(i) + p.recvline().decode())
