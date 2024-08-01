7eb626aa8b3b85adb883875d8ac5859aa7a1bc20
HenriMertens <149707229+HenriMertens@users.noreply.github.com>
2024-08-01 17:23:26 +0200

    Update checkpass_fast.py

--- rev/hard/checkpass/checkpass_fast.py
+++ rev/hard/checkpass/checkpass_fast.py
@@ -1,3 +1,5 @@
+#Same as checkpass_slow.py, but doesnt repeat alread guessed characters
+#For example 30/32 characters guessed -> slow still replaces all 32 chars whereas fast only checks remaining 2 chars
 import sys
 
 import string
@@ -8,7 +10,7 @@ flag_chars = string.digits + string.ascii_letters + '_'
 
 context.log_level = 'error'
 
-
+#save the indexes of all correctly guessed chars
 guessed = []
 
 def countinstructions(flag):
@@ -25,6 +27,7 @@ def replace_char(s, i, new_char):
         raise IndexError("Index out of range")
     return s[:i] + new_char + s[i + 1:]
 
+#check if the current index is already correctly guessed
 def already_guessed(i):
     if guessed.__contains__(i):
         return True
@@ -40,6 +43,8 @@ while any(x == '~' for x in password_guess):
     print('guessed so far are: ')
     print(guessed)
     print("i is {}".format(i))
+
+    # If the index is not already correctly guessed, try to guess it
     if not already_guessed(i):
         for c in flag_chars:
             new_guess = replace_char(password_guess, i, c)
@@ -48,10 +53,12 @@ while any(x == '~' for x in password_guess):
             if count > best_count:
                print("need a:" + c)
                password_guess = replace_char(password_guess, i, c)
+                # if you guess correctly, append it to the guessed array
                guessed.append(i)
         if(i == 31):
             i = -1
         i+=1
+    #if it is correctly guessed already just go to next index
     if already_guessed(i):
         if (i == 31):
             i = -1
